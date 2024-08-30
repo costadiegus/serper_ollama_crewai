4ec9681f11c959b72348af57e6c8d1bc9ff5b8dd
DG Costa <costadiegus@gmail.com>
2024-08-29 21:28:39 -0300

    _git.bat / .env.example

--- _git.bat
+++ _git.bat
@@ -2,6 +2,8 @@ git init
 git add .
 git commit -m "Primeiro commit - adicionando código Python"
 git remote add origin https://github.com/costadiegus/serper_ollama_crewai.git
+git branch -m master main
+git pull origin main --allow-unrelated-histories
 git push -u origin main
 git add .github/workflows/ci.yml
 git commit -m "Adiciona configuração do CI com Poetry"
